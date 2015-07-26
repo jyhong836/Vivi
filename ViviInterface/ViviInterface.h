e46f218c9d4562bb7320e277c9721d77c592df37
Junyuan Hong <jyhong836@gmail.com>
2015-07-27 00:48:40 +0800

    Rename targets

---
+++ ViviInterface/ViviInterface.h
@@ -0,0 +1,19 @@
+//
+//  ViviInterface.h
+//  ViviInterface
+//
+//  Created by Junyuan Hong on 7/27/15.
+//  Copyright © 2015 Junyuan Hong. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ViviInterface.
+FOUNDATION_EXPORT double ViviInterfaceVersionNumber;
+
+//! Project version string for ViviInterface.
+FOUNDATION_EXPORT const unsigned char ViviInterfaceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViviInterface/PublicHeader.h>
+
+
