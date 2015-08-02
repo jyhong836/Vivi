f0fb612bfbd3c153437d6955cde19924674cda33
Junyuan Hong <jyhong836@gmail.com>
2015-08-02 13:51:48 +0800

    Fix input view not clear after failed send

--- Vivi/InputViewController.swift
+++ Vivi/InputViewController.swift
@@ -22,16 +22,16 @@ class InputViewController: NSViewController {
     
     @IBAction func sendButtonClicked(sender: NSButton) {
         if let buddy = currentBuddy {
-            // FIXME: Didn't receive any message when fail to send message
             currentClient?.sendMessageToAccount(buddy,
                 message: inputTextView.textStorage?.string,
                 handler: { (errType) -> Void in
-                    if errType == VSClientErrorType.ClientUnavaliable {
-                        // TODO: Add notification here for error
-                        NSLog("Attend to send message from unavalible client: %@", (self.currentClient?.account.getFullAccountString())!)
-                    } else if errType == VSClientErrorType.None {
-                        self.inputTextView.textStorage?.setAttributedString( NSAttributedString(string: ""))
-                    }
+                    // TODO: Add notification here for error
+//                    if errType == VSClientErrorType.ClientUnavaliable {
+//                        // TODO: Add notification here for error
+//                        NSLog("Attend to send message from unavalible client: %@", (self.currentClient?.account.getFullAccountString())!)
+//                    } else if errType == VSClientErrorType.None {
+//                    }
+                    self.inputTextView.textStorage?.setAttributedString( NSAttributedString(string: ""))
                 }
             )
         }
