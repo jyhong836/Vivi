3d983054ca200badfe9e49257232ff3abc6637e0
Junyuan Hong <jyhong836@gmail.com>
2015-08-04 23:22:03 +0800

    Add vibrant effects.

---
+++ Vivi/MainWindowController.swift
@@ -0,0 +1,23 @@
+//
+//  MainWindowController.swift
+//  Vivi
+//
+//  Created by Junyuan Hong on 8/4/15.
+//  Copyright © 2015 Junyuan Hong. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        window?.titlebarAppearsTransparent = true
+        window?.titleVisibility = NSWindowTitleVisibility.Hidden
+        window?.movableByWindowBackground = true
+//        window?.movable = true
+    }
+
+}
